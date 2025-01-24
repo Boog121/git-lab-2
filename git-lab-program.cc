03646bf669a28d24b93ab8e1faba1c0f23003e8c
Bo Ogden <bo338621@ohio.edu>
2025-01-24 15:33:11 -0500

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Bo Ogden
+ *        Date: 01/24/2025
+ * Description: Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+   cout << "Hello Git!" << endl;
+
+   return 0;
+}
